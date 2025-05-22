9472d6ed7ad619aafabae4b13d5123c54433708c
Mohamed Mufeez <it22320896@my.sliit.lk>
2025-05-22 16:58:35 +0530

    fronteend works

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+   
+  );
+}
+
+export default App;
