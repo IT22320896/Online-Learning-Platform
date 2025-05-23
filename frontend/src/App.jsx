a9a4f64e4f922cb47befcbc5da39bc6631d797e9
Mohamed Mufeez <it22320896@my.sliit.lk>
2025-05-23 11:39:31 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
-
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
+import Dashboard from "./pages/Dashboard";
+import CoursesList from "./pages/CoursesList";
+import PrivateRoute from "./components/routes/PrivateRoute";
 import GuestRoute from "./components/routes/GuestRoute";
 import { AuthProvider } from "./contexts/AuthContext";
 import "./App.css";
@@ -15,6 +17,8 @@ function App() {
         <div className="flex flex-col min-h-screen w-full">
           <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
             <Routes>
+              <Route path="/" element={<Home />} />
+
               {/* Guest routes - redirect to dashboard if already logged in */}
               <Route
                 path="/login"
@@ -30,6 +34,17 @@ function App() {
                   <GuestRoute>
                     <Register />
                   </GuestRoute>
+                }
+              />
+
+              <Route path="/courses" element={<CoursesList />} />
+
+              <Route
+                path="/dashboard"
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
                 }
               />
             </Routes>
